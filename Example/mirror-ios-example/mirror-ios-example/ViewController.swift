9fc77ace3f38b2d6c9a1a5b7eadc42d6a6e022f8
terry lee <terry.lee@scmp.com>
2022-04-22 10:06:26 +0800

    UPDATED: Add example project and support cocoapods

---
+++ Example/mirror-ios-example/mirror-ios-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mirror-ios-example
+//
+//  Created by Terry Lee on 2022/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
